5042071b231f8d17751403447d3fa17a98878138
ThanhLuan0202 <thanhluan020203@gmail.com>
2025-03-26 10:17:04 +0700

    update

--- Tracio/Tracio/Program.cs
+++ Tracio/Tracio/Program.cs
@@ -86,6 +86,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 
 var app = builder.Build();
+app.Urls.Add("http://0.0.0.0:8080");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- Tracio/Tracio/obj/Debug/net8.0/Tracio.API.AssemblyInfo.cs
+++ Tracio/Tracio/obj/Debug/net8.0/Tracio.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tracio.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb90f84a2108c0a1748664d54c3177663dcec254")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91943c420accdbe31df78f92726d09e362b18ce3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tracio.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tracio.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
