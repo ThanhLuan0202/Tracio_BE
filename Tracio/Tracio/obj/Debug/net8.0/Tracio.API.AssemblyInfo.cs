1d5fca47326702d305ccf7a40de22d32febe6935
ThanhLuan0202 <thanhluan020203@gmail.com>
2025-03-26 10:24:07 +0700

    update

--- Tracio/Tracio/obj/Debug/net8.0/Tracio.API.AssemblyInfo.cs
+++ Tracio/Tracio/obj/Debug/net8.0/Tracio.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tracio.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91943c420accdbe31df78f92726d09e362b18ce3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5042071b231f8d17751403447d3fa17a98878138")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tracio.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tracio.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
